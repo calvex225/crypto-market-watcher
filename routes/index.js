0e30788f6e293e7a077a0e0143b60cf7d284c995
calvex225 <calvex.gbetie@gmail.com>
2018-01-21 20:39:08 +0000

    dashboard route seting

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,16 @@ import express from 'express';
 
 const router = express.Router();
 
-/* GET index page. */
+/* GET index/login page. */
 router.get('/', (req, res) => {
   res.render('index', {
+    title: 'Crypto Market Watcher'
+  });
+});
+
+/* GET dashboard page. */
+router.get('/dasboard', (req, res) => {
+  res.render('dasboard', {
     title: 'Crypto Market Watcher'
   });
 });
